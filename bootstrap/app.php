44dfc6c48a49f77462509fd4fa7b052661da4f0a
Jonathan10Diez <jonathan@paradisealacarte.com>
2026-05-16 18:13:06 +0200

    fix: forzar HTTPS en producción y confiar en proxies de Docker

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,11 +10,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-
-        // Idioma
-        $middleware->web(append: [\App\Http\Middleware\SetLocale::class,]);
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
